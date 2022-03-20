fff6e8908a988c5cca2c3800a3c0f0bd66a5f34c
youprog1227 <you20011227@gmail.com>
2022-03-20 16:14:01 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
+  
   root 'static_pages#home'
-
+  get '/help' => 'static_pages#help'
+  get '/about' => 'static_pages#about'
+  get '/contact' => 'static_pages#contact'
+  
+  get '/signup' => 'users#new'
+  
   get 'foos/bar'
-
   get 'foos/baz'
-
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
 end
